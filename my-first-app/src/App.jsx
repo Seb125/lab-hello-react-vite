27e2726d8e406da8aa82023426b3b84885035ff4
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2023-11-29 15:40:47 +0400

    kk

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,39 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div id="base">
+    <div className="App">
+    
+      
+      <nav id='navigation'>
+        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="Ironhack Logo"/>
+        <img  src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png'/>
+      </nav>
+
+      <h1>Say hello to ReactJS</h1>
+
+      <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.
+      
+      </p>
+
+      <button> Awesome!</button>
+
+
+
+    </div>
+
+    <div>
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png" alt="Ironhack Logo"/>
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png" alt="Ironhack Logo"/>
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png" alt="Ironhack Logo"/>
+      <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="Ironhack Logo"/>
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
